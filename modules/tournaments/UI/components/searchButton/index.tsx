05e0e6e935258c4e5e16416a96c70f2ccdfca1fd
VitVin <Pochtavivo@gmail.com>
2022-02-12 20:44:53 +0200

    CreatedFlatList and pagination

--- modules/tournaments/UI/components/searchButton/index.tsx
+++ modules/tournaments/UI/components/searchButton/index.tsx
@@ -2,7 +2,7 @@ import React, { FC, } from 'react';
 import { styles } from './style';
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { useState } from 'react';
-import { Pressable, Text } from 'react-native';
+import { Pressable, Text, TouchableOpacity } from 'react-native';
 
 interface IProps {
     onPress: () => void;
@@ -11,8 +11,8 @@ interface IProps {
 export const SearchButton: FC<IProps> = ({ onPress }) => {
 
     return (
-        <Pressable style={styles.container} onPress={onPress}>
+        <TouchableOpacity style={styles.container} onPress={onPress}>
             <Text>Apply filters</Text>
-        </Pressable>
+        </TouchableOpacity>
     );
 };
