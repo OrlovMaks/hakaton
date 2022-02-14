23ded7ba48469a57664288fe26cdc33535353550
OrlovMaks <93343100+OrlovMaks@users.noreply.github.com>
2022-02-14 07:00:32 +0200

    add reg participents

--- modules/tournaments/UI/components/tournamentsList/index.tsx
+++ modules/tournaments/UI/components/tournamentsList/index.tsx
@@ -4,9 +4,10 @@ import { View, TouchableOpacity, Text, FlatList, Image, Pressable, SafeAreaView 
 import { ThemesContext } from '../../../../../src/themes';
 import { LocalizationContext } from '../../../../../src/localization';
 import { styles } from './style';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectTableInformation } from '../../../../../src/appStore/redux/tableInformationState/tableInformationSelector';
 import { setTableInfoAction } from '../../../../../src/appStore/redux/tableInformationState/tableInformationActions';
+import { selectUserData } from '../../../../../src/appStore/redux/authenticationState/authenticationStateSelector';
 
 const axios = require("axios");
 
@@ -22,25 +23,48 @@ export const TournamentsList: FC<IProps> = memo(({ navigation, data },) => {
     const LocalContext = useContext(LocalizationContext)
     const theme = useContext(ThemesContext);
     const dispatch = useDispatch()
+    const userData = useSelector(selectUserData)
 
     const setInformation = (tournament) => {
         dispatch(setTableInfoAction(tournament))
         navigation.navigate('Matches')
     }
 
+    const registrationToTournament = async (tournamentId, uid, client, accessToken) => {
+        console.log(tournamentId, uid, client, accessToken)
+
+
+        try {
+            let response = await axios.post(`https://tournament-t.herokuapp.com/users_tournaments`,
+                {
+                    headers: {
+                        'access-token': accessToken,
+                        'client': client,
+                        'uid': uid,
+                    }
+                },
+                {
+                    tournament_id: tournamentId
+                }
+            );
+            console.log(response.data)
+        }
+
+        catch (error) {
+            console.log(error)
+        }
+    }
 
     const Item = ({ tournament }) => (
         <TouchableOpacity style={[styles.item, { backgroundColor: theme.colors.LIST_ITEMS }]} onPress={() => setInformation(tournament)}>
             <View style={styles.tournamentInfoButton}>
                 <View>
                     <Text style={styles.itemText}>{tournament.name}</Text>
-
                 </View>
                 <View>
-
                 </View>
             </View>
-            <TouchableOpacity style={[styles.registrationButton, { backgroundColor: theme.colors.BUTTON_COLOR }]} >
+            <TouchableOpacity style={[styles.registrationButton, { backgroundColor: theme.colors.BUTTON_COLOR }]} onPress={() => registrationToTournament(tournament.id, userData.uid, userData.client, userData.accessToken)}>
                 <Text style={{ color: theme.colors.TEXT_COLOR }}>{LocalContext.translations.PARTICIPATE_TITLE}</Text>
             </TouchableOpacity>
         </TouchableOpacity >
--- modules/tournaments/useCases/setScore/index.ts
+++ modules/tournaments/useCases/setScore/index.ts
@@ -5,7 +5,7 @@ export const sendSetScoreRequest = async (matchId :string, scoreUser1: string, s
     try {
         console.log(matchId, scoreUser1, scoreUser2, status)
         console.log(`https://tournament-t.herokuapp.com/matches/`+matchId)
-        let response = await axios.put(`https://tournament-t.herokuapp.com/matches/`+matchId,
+        let response = await axios.put(`https://tournament-t.herokuapp.com/matches/${matchId}`,
             {
                 score_user_1: scoreUser1,
                 score_user_2: scoreUser2,
